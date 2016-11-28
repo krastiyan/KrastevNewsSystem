cc25b392443b235fed0678a77fc2654234e5d5dd
PC <PC@FJPC-19>
2016-11-28 13:14:27 +0200

    Add project files.

---
+++ KrastevNewsSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KrastevNewsSystem.Startup))]
+namespace KrastevNewsSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
